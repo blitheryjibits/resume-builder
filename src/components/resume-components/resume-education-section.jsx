dc61ef17ad6277e8673f762aa90e53b6be9ec16a
robert thornton <robert.thornton92@gmail.com>
2025-03-05 09:14:36 +0200

    Refactor: created a dynamic mapping function to create input fields and removed hardcoded fields. Adjusted proptypes by removing redundant fields.

--- src/components/resume-components/resume-education-section.jsx
+++ src/components/resume-components/resume-education-section.jsx
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 export default function EducationSection({ data }) {
   return (
     <div className="education">
+      <h3 className="section-title">Education</h3>
       {data.map((course, index) => (
-        <div key={index} data-key={course.id}>
-          <p>{course.schoolName}</p>
-          <p>{course.degree}</p>
-          <p>{course.fieldOfStudy}</p>
-          <p>{course.graduationYear}</p>
+        <div key={index} data-key={course.id} className={'section-container'}>
+          {Object.keys(course).map((key) => (
+            key !== 'id' && course[key] && <p key={key} className={`section-${key}`}>{`${course[key]}`}</p>
+          ))}
         </div>
       ))}
     </div>
@@ -18,10 +18,8 @@ export default function EducationSection({ data }) {
 EducationSection.propTypes = {
   data: PropTypes.arrayOf(
     PropTypes.shape({
-      schoolName: PropTypes.string,
-      degree: PropTypes.string,
-      fieldOfStudy: PropTypes.string,
-      graduationYear: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      // No need to explicitly list other fields, make it flexible
     })
-  ),
+  ).isRequired,
 };
