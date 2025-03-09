a8d32048fafeaec47714080e2f0d2bb4479ae6fc
robert thornton <robert.thornton92@gmail.com>
2025-03-09 22:54:43 +0200

    Updated: changed the degree field key to GPA to improve realistic resume education information.

--- src/components/form-components/createCourse.jsx
+++ src/components/form-components/createCourse.jsx
@@ -3,7 +3,7 @@ export default function createCourse(course = {}) {
     const newCourse = {
         schoolName: course.schoolName ?? "",
         degree: course.degree ?? "",
-        fieldOfStudy: course.fieldOfStudy ?? "",
+        GPA: course.GPA ?? "",
         graduationYear: course.graduationYear ?? "",
         id: course.id ?? ""
     }
