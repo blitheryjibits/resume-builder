b9d85f9c5d1f2d8298e19ee5e2b0b7459eaa3211
robert thornton <robert.thornton92@gmail.com>
2025-03-19 23:26:09 +0200

    Refactor: changed the class names to match the changes made in the react modules i.e., to handle the change from camelCase to hyphenated-strings.

--- src/components/resume-components/resume-header-section.jsx
+++ src/components/resume-components/resume-header-section.jsx
@@ -2,11 +2,11 @@ import PropTypes from "prop-types";
 export default function HeaderSection({ className, data }) {
   return (
     <div className={className}>
-      <h2 className="client-name">{data.firstName} {data.lastName}</h2>
+      <h2 className="client-name">{data["First-Name"]} {data["Last-Name"]}</h2>
         <div className="contact-info">
         {Object.entries(data).map(([key, value], index) => {
           const isLast = index === Object.keys(data).length - 1;``
-          if (key !== "firstName" && key !== "lastName" && value) {
+          if (key !== "First-Name" && key !== "Last-Name" && value) {
             return <span key={key}>
               {value}
               {!isLast && <>&nbsp;|&nbsp;</>}
