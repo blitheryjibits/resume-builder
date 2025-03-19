8b1f84aabcc9e39c9a28b735ad8c5b82913d7358
robert thornton <robert.thornton92@gmail.com>
2025-03-19 23:21:15 +0200

    Refactor: changed object keys from camelCase to hyphented-strings that can be easily separated and displayed as the values in the form labels

--- src/components/form-components/create-job.jsx
+++ src/components/form-components/create-job.jsx
@@ -1,10 +1,11 @@
 export default function createJob(job = {}) {
     const newJob = {
-        companyName: job.companyName ?? "",
-        jobTitle: job.jobTitle ?? "",
-        startDate: job.startDate ?? "",
-        endDate: job.endDate ?? "",
-        id: job.id ?? "",
+        "Job-Title": job["Job-Title"] ?? "",
+        "Company-Name": job["Company-Name"] ?? "",
+        "Start-Date": job["Start-Date"] ?? "",
+        "End-Date": job["End-Date"] ?? "",
+        "Responsibilities": job["Responsibilities"] ?? "",
+        id: job.id ?? 0,
     }
     return newJob;
 }
