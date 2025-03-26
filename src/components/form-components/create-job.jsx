9791c1e1756533d20aeb767ee35bdd05fc2a3f2c
robert thornton <robert.thornton92@gmail.com>
2025-03-26 20:54:34 +0200

    Changed: moved the company name field below the start and end dates for styling.

--- src/components/form-components/create-job.jsx
+++ src/components/form-components/create-job.jsx
@@ -1,9 +1,9 @@
 export default function createJob(job = {}) {
     const newJob = {
         "Job-Title": job["Job-Title"] ?? "",
-        "Company-Name": job["Company-Name"] ?? "",
         "Start-Date": job["Start-Date"] ?? "",
         "End-Date": job["End-Date"] ?? "",
+        "Company-Name": job["Company-Name"] ?? "",
         "Responsibilities": job["Responsibilities"] ?? "",
         id: job.id ?? 0,
     }
