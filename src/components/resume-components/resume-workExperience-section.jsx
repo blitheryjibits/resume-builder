7c8e67da2a6552d1ed3733e0f3ff7090a1f7b068
robert thornton <robert.thornton92@gmail.com>
2025-03-26 20:59:28 +0200

    Added: specific style for textarea used in the form where user inputs job responsibilities.

--- src/components/resume-components/resume-workExperience-section.jsx
+++ src/components/resume-components/resume-workExperience-section.jsx
@@ -1,14 +1,30 @@
 import PropTypes from 'prop-types';
 
 export default function WorkExperienceSection({ data }) {
+  // let res = (data[0]).Responsibilities.split("\n");
+  // console.log(res);
   return (
     <div className="jobs">
       <h3 className="section-title">Work Experieince</h3>
       {data.map((job, index) => (
         <div key={index} data-key={job.id} className={'section-container'}>
-          {Object.keys(job).map((key) => (
-            key !== 'id' && job[key] && <p key={key} className={`section-${key}`}>{`${job[key]}`}</p>
-          ))}
+           {Object.keys(job).map((key) => {
+            if (key === 'Responsibilities' && job[key]) {
+              // Render responsibilities as a <ul> with <li> items
+              const responsibilitiesArray = job[key].split('\n'); // Split by newlines
+              return (
+                <ul key={key} className={`section-${key}`}>
+                  {responsibilitiesArray.map((item, idx) => (
+                    <li key={idx}>{item}</li>
+                  ))}
+                </ul>
+              );
+            } else if (key !== 'id' && job[key]) {
+              // Render other fields as <p>
+              return <p key={key} className={`section-${key}`}>{job[key]}</p>;
+            }
+            return null;
+          })}
         </div>
       ))}
     </div>
@@ -21,4 +37,5 @@ WorkExperienceSection.propTypes = {
       id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
     })
   ).isRequired,
+  
 };
