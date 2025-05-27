3895613174522b9e4a1fbdf2995d07c4ac72da6e
robert thornton <robert.thornton92@gmail.com>
2025-05-27 11:00:12 +0300

    Added: import for style sheet for project section in resume

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./styles/app.css";
 import "./styles/form-styles.css";
 // import "./styles/resumeStyle.css";
+import "./styles/project-styles.css";
 import "./styles/app-styles.css";
 import MyForm from "./components/form-state-controller";
 import MyResume from "./components/resume-components/myResume";
